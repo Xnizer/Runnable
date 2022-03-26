e132d4d2fcc5a73cb022b0a79fc40f388fcadb50
Xnizer <saeid.zain.aldeen@gmail.com>
2022-03-26 10:04:53 +0100

    Add example.

---
+++ Runnable.Example/Examples/Die.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Runnable.Attributes;
+
+namespace Runnable.Example
+{
+    internal class Die
+    {
+        Random _random = new Random();
+
+        public Die(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public int Roll()
+        {
+            return _random.Next(1, 7);
+        }
+
+        [Runnable("Roll 10 dice", ConstructorParameters = new object[] { 1337 })]
+        public void TestRoll()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(Roll());
+        }
+    }
+}
