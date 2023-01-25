c086b73cde4fd00220c6e738eaecf74be12371ed
Xnizer <saeid.zain.aldeen@gmail.com>
2023-01-25 08:19:10 -0800

    Add ITerminal interface.

---
+++ Runnable/IO/ITerminal.cs
@@ -0,0 +1,10 @@
+﻿namespace Runnable.IO
+{
+    public enum MessageType { Text, Error }
+    public interface ITerminal
+    {
+        void PrintMessage(string message, MessageType type);
+        void Clear();
+        bool ReadInt(int min, int max, out int value);
+    }
+}
