c3ca830c00d571bf056f662ed4796fc80f8646c7
Xnizer <saeid.zain.aldeen@gmail.com>
2023-01-25 08:22:44 -0800

    Add an implementation for ITerminal that uses System.Console.

---
+++ Runnable/IO/ConsoleTerminal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Runnable.IO
+{
+    internal class ConsoleTerminal : ITerminal
+    {
+        public void PrintMessage(string message, MessageType type)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+
+            switch (type)
+            {
+                case MessageType.Text:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case MessageType.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                default:
+                    Console.ForegroundColor = originalColor;
+                    break;
+            }
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = originalColor;
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public bool ReadInt(int min, int max, out int value)
+        {
+            while (true)
+            {
+                var keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                    break;
+
+                if (int.TryParse(keyInfo.KeyChar.ToString(), out value))
+                    if (value >= min && value <= max)
+                        return true;
+            }
+
+            value = 0;
+            return false;
+        }
+    }
+}
