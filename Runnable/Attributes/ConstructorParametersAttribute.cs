dbe28bb22cfae396152a88105cb5e92dbee6ebae
Xnizer <saeid.zain.aldeen@gmail.com>
2023-01-26 05:33:50 -0800

    Add attribute classes for constructor and method parameters, so they can be removed from 'RunnableAttribute'.

---
+++ Runnable/Attributes/ConstructorParametersAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Runnable.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ConstructorParametersAttribute : Attribute
+    {
+        public object[] Parameters { get; }
+
+        public ConstructorParametersAttribute(params object[] parameters)
+        {
+            Parameters = parameters;
+        }
+    }
+}
---
+++ Runnable/Attributes/MethodParametersAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Runnable.Attributes
+{
+    public class MethodParametersAttribute : Attribute
+    {
+        public object[] Parameters { get; }
+
+        public MethodParametersAttribute(params object[] parameters)
+        {
+            Parameters = parameters;
+        }
+    }
+}
